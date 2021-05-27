4be9632408019a06839723a74131c3f191f3b011
vitoriaroas <vitoriaroas@gmail.com>
2021-05-27 11:59:32 -0400

    added css for header and background

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './scenes/Home'
 import Login from './scenes/Login'
 import Signup from './scenes/Signup'
+import Header from './components/Header'
+import Stream from './img/stream.jpg'
 
 function App() {
   return (
     <div>
       <Router>
+        <Header />
         <Switch>
           <Route path="/Login" component={Login} />
           <Route path="/Signup" component={Signup} />
           <Route path="/Home" component={Home} />
         </Switch>
+        <img src={Stream} alt="Stream" />
+       {/* <Footer style={{ textAlign: 'center' }}>©2021 Created by Vitoria Roas</Footer>  */}
       </Router>
     </div>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          BREATHE
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDarkDropdown"
+          aria-controls="navbarNavDarkDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDarkDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul
+                class="dropdown-menu dropdown-menu-dark"
+                aria-labelledby="navbarDarkDropdownMenuLink"
+              >
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Sign-up
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Login
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
