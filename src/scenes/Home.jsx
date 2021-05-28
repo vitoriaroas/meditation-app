a0c9ac6df343c8355c3ca07b250464fa75f546e5
vitoriaroas <vitoriaroas@gmail.com>
2021-05-28 18:11:05 -0400

    added the stop

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -12,26 +12,14 @@ function Home() {
    audio.pause()
  }
   return (
-    <>
-      <h1 class="container">Welcome</h1>
-      <div class="container">
-        <Button onClick={start} variant="dark">
-          Start
-        </Button>{' '}
-        <Button onClick={start} variant="dark">
-          5 min
-        </Button>{' '}
-        <Button onClick={start} variant="dark">
-          10 min
-        </Button>{' '}
-        <Button onClick={start} variant="dark">
-          15 min
-        </Button>{' '}
-        <Button onClick={stop} variant="dark">
-          Stop
-        </Button>{' '}
+    <section className="hero">
+      <div className="welcome">Welcome</div>
+      <div className="buttonArea">
+        <Button onClick={start} variant="dark"> Start</Button>
+        <Button onClick={stop} variant="dark">Stop</Button>
       </div>
-    </>
+      </section>
+   
   )
 }
 
